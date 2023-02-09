4c3c3981ac188f2986f71dbdeba8ab54231bcb54
academy-glo <timjimmik12@gmail.com>
2023-02-09 13:54:05 +0200

    first commit

---
+++ js/swiper.js
@@ -0,0 +1,30 @@
+// "use strict"
+
+
+// const reviewsSwiper = document.querySelector('.portfolio__slider');
+
+// if (reviewsSwiper) {
+//   	const swiper = new Swiper('.portfolio__slider', {
+//     		// Optional parameters
+//         loop: true,
+//         observer: true,
+//         slidesPerView: 3,
+//         speed:800,
+      
+//         // observeParents: true,
+//         // autoHeight: true,
+//         // spaceBetween: 56,
+//        // Navigation arrows
+//         navigation: {
+//           nextEl: '.portfolio__arrow-right',
+//           prevEl: ' .portfolio__arrow-left',
+//           },
+      
+//       	});
+//       }
+      
+    //   <link
+    //   rel="stylesheet"
+    //   href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"
+    // />
+      //  <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script> 
